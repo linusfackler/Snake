6fa14b7fa582be27ac4bebae94274109f1fac6f2
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-05-17 14:17:20 +0200

    update

---
+++ Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Snake : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+    private float horizontal;
+    private float vertical;
+
+
+    public void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+        vertical = context.ReadValue<Vector2>().y;
+    }
+}
